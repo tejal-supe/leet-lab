3bd61bf9cfa921639c7af88ef622220a6e2996d2
tejal-supe <supetejalc@gmail.com>
2025-04-23 00:42:05 +0530

    Prisma basic setup and db migration for usermodel

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config()
+const PORT = process.env.PORT;
 
 const app = express();
+app.use(express.json())
 
-app.listen((process.env.PORT), () => {
+app.listen((PORT), () => {
     console.log(`Server running on port ${PORT}`);
 })
