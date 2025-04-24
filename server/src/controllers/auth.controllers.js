856f02227655d31025c381e4a1e64ca5a45b5129
tejal-supe <supetejalc@gmail.com>
2025-04-25 01:10:52 +0530

    Register route and controller for auth module

---
+++ server/src/controllers/auth.controllers.js
@@ -0,0 +1,56 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+
+export const register = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const exisitingUser = await db.user.findUnique({
+      where: { email },
+    });
+    if (exisitingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: UserRole.USER,
+      },
+    });
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+        image: newUser.image,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating user", error);
+    res.status(500).json({
+      error: "Error Creating User",
+    });
+  }
+};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
+export const check = async (req, res) => {};
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config()
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Helllo very excited!!")
+})
+
+app.use(("/api/v1", authRoutes));
+
 
 app.listen((PORT), () => {
     console.log(`Server running on port ${PORT}`);
