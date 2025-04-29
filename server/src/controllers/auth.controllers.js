a3c31664926bff9385197bc6d88802365c4b9127
tejal-supe <supetejalc@gmail.com>
2025-04-29 07:28:34 +0530

    Logout, check controller completed along with auth middleware

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -54,20 +54,20 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await db.user.findUnique({
-      where:{email}
-    })
+      where: { email },
+    });
     if (!user) {
       return res.status(401).json({
-        error:"User not found"
-      })
+        error: "User not found",
+      });
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({
-        error:"Invalid credentials"
-      })
+        error: "Invalid credentials",
+      });
     }
-    const token = jwt.sign({id:user.id}, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
     res.cookie("jwt", token, {
@@ -87,13 +87,40 @@ export const login = async (req, res) => {
       },
     });
   } catch (error) {
-    console.error(error, 'Error in login controller');
+    console.error(error, "Error in login controller");
     res.status(500).json({
-      error:"Error while login"
-    })
-    
+      error: "Error while login",
+    });
   }
 };
 
-export const logout = async (req, res) => {};
-export const check = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.error(error, "Error logout controller");
+    res.status(500).json({
+      error: "Error logging user out",
+    });
+  }
+};
+
+export const check = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User authenticated successfully",
+      user:req.user
+    });
+  } catch (error) {
+    console.error(error, "Error check controller");
+    res.status(500).json({
+      error: "Error checking",
+    }); 
+  }
+};
---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import { db } from "../libs/db.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({
+        message: "Unathorized - No token provided",
+      });
+    }
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res.status(401).json({
+        message: "Unathorized - Invalid token",
+      });
+    }
+    const user = await db.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+      select: {
+        id: true,
+        image: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error, "Auth middleware error");
+    res.status(500).json({
+      error: "Error authenticating user",
+    });
+  }
+};
